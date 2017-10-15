77eee133758f2e11172941a54f3d3ad4f5c3c5b5
amberkar <amberkar@umich.edu>
2017-10-15 13:34:08 -0400

    part 1

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -21,3 +21,44 @@ for images in img_list:
 		print(images['alt'])
 	except:
 		print("No alternative text provided!")
+
+######### PART 1 #########
+
+try:
+  gov_data = open("gov_data.html",'r').read()
+except:
+  gov_data = requests.get("https://www.nps.gov/index.htm").text
+  f = open("gov_data.html",'w')
+  f.write(gov_data)
+  f.close()
+
+gov = BeautifulSoup(gov_data, 'html.parser')
+
+links_ = gov.find_all('a')
+
+state_part = gov.find("ul",{"class":"dropdown-menu SearchBar-keywordSearch"})
+
+all_= state_part.find_all('a')
+
+states = ['Michigan', 'Arkansas', 'California']
+for item in all_:
+  state_name = item.text
+  if state_name in states:
+    state_link = "https://www.nps.gov" + item.get('href')
+    file_n = state_name.lower() + "_data.html"
+    try:
+      state_name_ = open(file_n, 'r').read()
+    except:
+      state_name_ = requests.get(state_link).text
+      f = open(file_n,'w')
+      f.write(state_name_)
+      f.close()
+
+michigan_data = open("michigan_data.html", "r").read()
+michigan_soup = BeautifulSoup(michigan_data, 'html.parser')
+
+arkansas_data = open("arkansas_data.html", "r").read()
+arkansas_soup = BeautifulSoup(arkansas_data, 'html.parser')
+
+california_data = open("california_data.html", "r").read()
+california_soup = BeautifulSoup(california_data, 'html.parser')
